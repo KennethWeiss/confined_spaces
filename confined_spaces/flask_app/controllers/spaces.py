62b0da17d7df29d1ef9ee9552afc064a52625ce4
Kenny Weiss <kenny1weiss@aol.com>
2023-06-07 11:43:31 -0700

    Add files via upload
    Displays users spaces

---
+++ confined_spaces/flask_app/controllers/spaces.py
@@ -0,0 +1,14 @@
+from flask_app import app
+from flask import render_template, redirect, request, url_for, session, flash
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+from flask_app.models import space
+
+@app.route("/userspaces")
+def display_user_spaces():
+    spaces = space.Space.get_users_spaces()
+    return render_template("wall.html", spaces=spaces)
+
+@app.route("/addspace")
+def add_space():
+    return render_template("create_space.html")
---
+++ confined_spaces/flask_app/models/space.py
@@ -0,0 +1,66 @@
+from flask import flash
+from flask_app.config.mysqlconnection import connectToMySQL
+
+class Space:
+    db = "confined_spaces"
+    def __init__(self, data):
+        self.id = data['id']
+        self.name = data['name']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def create(cls,data):
+        query = """INSERT INTO confined_spaces (name) 
+                VALUES (%(name)s);"""
+        results = connectToMySQL(cls.db).query_db(query,data)
+        return results
+    
+    @classmethod
+    def get_users_spaces(cls):
+        query = """
+                SELECT confined_spaces.id, confined_spaces.name, confined_spaces.created_at, confined_spaces.updated_at FROM confined_spaces.confined_spaces
+                JOIN confined_spaces.users_has_confined_spaces
+                ON confined_spaces.confined_spaces.id = users_has_confined_spaces.confined_space_id
+                JOIN users
+                ON users_has_confined_spaces.user_id = users.id;
+                """
+        results = connectToMySQL(cls.db).query_db(query)
+        spaces = []
+        for space in results:
+            spaces.append(cls(space))
+        return spaces
+    
+    @staticmethod
+    def is_valid(user_dict):
+        is_valid = True
+        if len(user_dict["first_name"]) < 2:
+            is_valid = False
+            flash("First name should have at least 2 characters", "registration")
+        if len(user_dict["last_name"]) < 2:
+            is_valid = False
+            flash("Last name should have at least 2 characters", "registration")
+        if len(user_dict["email"]) < 2:
+            is_valid = False
+            flash("Email should have at least 2 characters", "registration")
+        if len(user_dict["password"]) < 2:
+            is_valid = False
+            flash("Password should have at least 2 characters", "registration")
+        if user_dict["password_confirmation"] != user_dict["password"]:
+            is_valid = False
+            flash("Password must match password confirmation", "registration")
+        return is_valid
+    
+    @staticmethod
+    def valid_login(user_dict):
+        is_valid = True
+        if len(user_dict["email"]) < 2:
+            is_valid = False
+            flash("Email should have at least 2 characters")
+        if len(user_dict["password"]) < 2:
+            is_valid = False
+            flash("Password should have at least 2 characters")
+        # if user_dict["password_confirmation"] != user_dict["password"]:
+        #     is_valid = False
+        #     flash("Password must match password confirmation")
+        return is_valid 
